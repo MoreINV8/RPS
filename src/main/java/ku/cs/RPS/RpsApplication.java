6c78df7b57ef8cef76de0ffc69354ce10b39deb1
INV8 <chaiyapat.sr@ku.th>
2024-10-17 20:28:32 +0700

    init project

---
+++ src/main/java/ku/cs/RPS/RpsApplication.java
@@ -0,0 +1,13 @@
+package ku.cs.RPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ku/cs/RPS/RpsApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.cs.RPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
