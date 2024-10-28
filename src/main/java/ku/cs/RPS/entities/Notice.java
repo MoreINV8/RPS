248f4f5b77084285affbf89273106606f4666943
INV8 <chaiyapat.sr@ku.th>
2024-10-28 13:14:39 +0700

    sent data to customer (usecase 12)

--- src/main/java/ku/cs/RPS/entities/Notice.java
+++ src/main/java/ku/cs/RPS/entities/Notice.java
@@ -8,27 +8,27 @@ import lombok.NoArgsConstructor;
 public class Notice {
 
     private String id;  // Renamed from noticeId to id
-    private String delivery_id;
-    private String driver_id;
-    private String car_registration;
-    private String start_work_date;
-    private String complete_status;
+    private String deliveryId;
+    private String driverId;
+    private String carRegistration;
+    private String startWorkDate;
+    private String completeStatus;
     private String employeeFirstName;
     private String employeeLastName;
 
-    public Notice (String id, String delivery_id, String driver_id, String car_registration, String start_work_date, String complete_status, String employeeFirstName, String employeeLastName){
+    public Notice(String id, String deliveryId, String driverId, String carRegistration, String startWorkDate, String completeStatus, String employeeFirstName, String employeeLastName) {
         this.id = id;
-        this.delivery_id = delivery_id;
-        this.driver_id = driver_id;
-        this.car_registration = car_registration;
-        this.start_work_date = start_work_date;
-        this.complete_status = complete_status;
+        this.deliveryId = deliveryId;
+        this.driverId = driverId;
+        this.carRegistration = carRegistration;
+        this.startWorkDate = startWorkDate;
+        this.completeStatus = completeStatus;
         this.employeeFirstName = employeeFirstName;
         this.employeeLastName = employeeLastName;
     }
 
     public String getDriverId() {
-        return driver_id;
+        return driverId;
     }
 
 }
--- src/main/java/ku/cs/RPS/mappers/NoticeMapper.java
+++ src/main/java/ku/cs/RPS/mappers/NoticeMapper.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 public class NoticeMapper implements RowMapper<Notice> {
     @Override
     public Notice mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -13,11 +14,11 @@ public class NoticeMapper implements RowMapper<Notice> {
         // id, delivery_id, driver_id, car_registration, start_work_date, complete_status
         Notice notice = new Notice();
         notice.setId(rs.getString("id"));
-        notice.setDelivery_id(rs.getString("delivery_id"));
-        notice.setDriver_id(rs.getString("driver_id"));
-        notice.setCar_registration(rs.getString("car_registration"));
-        notice.setStart_work_date(rs.getString("start_work_date"));
-        notice.setComplete_status(rs.getString("complete_status"));
+        notice.setDeliveryId(rs.getString("delivery_id"));
+        notice.setDriverId(rs.getString("driver_id"));
+        notice.setCarRegistration(rs.getString("car_registration"));
+        notice.setStartWorkDate(rs.getString("start_work_date"));
+        notice.setCompleteStatus(rs.getString("complete_status"));
         notice.setEmployeeFirstName(rs.getString("employee_first_name"));  // Set first name
         notice.setEmployeeLastName(rs.getString("employee_last_name"));    // Set last name
 
